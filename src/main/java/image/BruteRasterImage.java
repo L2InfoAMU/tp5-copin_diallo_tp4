109c959298ef95c96f7643d08acd1daf954923a5
c17000740 <kevcpn@gmail.com>
2018-11-26 16:14:35 +0100

    creation bruterasterimage + 2 constructeurs

---
+++ src/main/java/image/BruteRasterImage.java
@@ -0,0 +1,35 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class BruteRasterImage implements Image {
+    private int width, height;
+    private Color color;
+    private Color[][] colors;
+    private int x, y;
+
+    public BruteRasterImage(Color color, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public BruteRasterImage(Color[][] colors) {
+        this.colors[x][y] = colors[x][y];
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
